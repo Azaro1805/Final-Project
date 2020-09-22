e8d5e9ddb292ab20d6e53c1eaea4b6d7f7ee7a44
Or Azar <67314843+Azaro1805@users.noreply.github.com>
2020-09-22 14:36:53 +0300

    GraphsTest.py

---
+++ GraphsTest.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+import random
+
+gr2 = nx.gnp_random_graph(10, 0.4, seed=354, directed=False)
+nx.draw(gr2, with_labels=True)
+pos=nx.spring_layout(gr2)
+
+
+labels = {}
+for x in range(len(gr2)):
+    labels[x] = random.choice(['A', 'B', 'C'])
+
+print("Labels :", labels)
+
+neighbors = gr2.edges()
+print ( "the neighbors : ", neighbors)
+
+nx.draw_networkx_labels(gr2,pos,labels,font_size=5)
+plt.show()
