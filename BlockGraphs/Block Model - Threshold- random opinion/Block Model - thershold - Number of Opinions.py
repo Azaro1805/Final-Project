bf7325129d7fb22e13aca71624c3a8e7cbb5c198
Or azar <orazar1805@gmail.com>
2020-12-23 17:31:54 +0200

    Update Block Model - thershold - Number of Opinions.py

--- BlockGraphs/Block Model - Threshold- random opinion/Block Model - thershold - Number of Opinions.py
+++ BlockGraphs/Block Model - Threshold- random opinion/Block Model - thershold - Number of Opinions.py
@@ -6,8 +6,6 @@ import numpy as np
 import copy
 from collections import defaultdict
 
-random.seed(364)
-
 '''get two arrays and return the winner name'''
 def getWinner (winnerVotes,typeOfVotes):
     winners2 =""
@@ -44,6 +42,10 @@ def creatOpinions(BlockGraph, typeOfVotes, Opinions2, winnerVotes):
     Opinions = copy.deepcopy(Opinions2)
     return Opinions
 
+def createCom (numberOfCom,MinPeople, MaxPeople):
+    for i in range(numberOfCom):
+        sizes[i] = random.randint(MinPeople, MaxPeople)
+
 def Countvotes(typeOfVotes, winnerVotes, Opinions2,x):
 
     for i3 in range(len(typeOfVotes)):
@@ -56,8 +58,6 @@ def setFridends(edges, friends):
         pair = list(edges.keys())[d]
         friends[pair[0]].add(pair[1])
         friends[pair[1]].add(pair[0])
-    '''for e in range(len(friends)):
-        friends[e].remove(-1)'''
 
 def runInit(friends, votes, votes2):
     friends2 = copy.deepcopy(friends)
@@ -93,11 +93,12 @@ def percentOfVotes(counter,typeOfVotes,votes2,threshold,Opinions,f,change):
             '''need to change opinion if A != B , percent > threshold'''
             if (votes2[f][h2] >= threshold and typeOfVotes[h2] != Opinions[f]):
                 change = changeOpinion(votes2, threshold, typeOfVotes, Opinions, f, h2, change)
+                return change
     return change
 
 def changeOpinion(votes2,threshold,typeOfVotes,Opinions,f,h2,change):
        change = True
-       print("new opinion:", typeOfVotes[h2],"   old opinion:", Opinions[f], "  friend:",f, "   precent:",votes2[f][h2] )
+       #print("new opinion:", typeOfVotes[h2],"   old opinion:", Opinions[f], "  friend:",f, "   precent:",votes2[f][h2] )
        Opinions[f]= typeOfVotes[h2]
        return change
 
@@ -118,7 +119,7 @@ def CreatePlotGraph (changeVar, TotalIter , Xlegend , Ylabel):
     plt.show()
 
 def CreatescatterGraph (changeVar, WinnerGraph , Xlegend , Ylabel):
-    plt.scatter(changeVar, WinnerGraph)
+    plt.scatter(changeVar,WinnerGraph,label = ("The Start Winner is: "+ winnerStart) )
     plt.xlabel(Xlegend)
     plt.ylabel(Ylabel)
     plt.legend()
@@ -129,141 +130,111 @@ def prints (typeOfVotes, winnerVotes, winnerStart, Opinions2, edges):
     print(np.matrix(typeOfVotes))
     print(np.matrix(winnerVotes))
     print("Start winner is:",winnerStart)
-    print(" opinions:")
-    print (Opinions2 )
-    print("edges :")
-    print(edges)
+    #print(" opinions:")
+    #print (Opinions2 )
+    #print("edges :")
+    #print(edges)
 
-def printStartWinner ():
-    print(np.matrix(typeOfVotes))
-    print(np.matrix(winnerVotes))
-    print("Start winner is:", winnerStart)
+def getValue(a):
+    for b in a:
+        x=b
+        a.remove(b)
+        return  x
 
-def numberOfVotes (numberOfVotes):
-    if(numberOfVotes == 2):
-        typeOfVotes = ["A", "B"]
-        return typeOfVotes
-    if (numberOfVotes == 3):
-        typeOfVotes = ["A", "B" , "C"]
-        return typeOfVotes
-    if (numberOfVotes == 4):
-        typeOfVotes = ["A", "B" , "C", "D"]
-        return typeOfVotes
-    if (numberOfVotes == 5):
-        typeOfVotes = ["A", "B" , "C", "D" , "E"]
-        return typeOfVotes
-    if (numberOfVotes == 6):
-        typeOfVotes = ["A", "B" , "C", "D" , "E" ,"F"]
-        return typeOfVotes
-
+max_of_iter= 10
+numberOfCom = 20
+MinPeople = 45
+MaxPeople = 55
+sizes = [ 0 for i in range(numberOfCom) ]
+probs = [ [ 0 for i in range(numberOfCom) ] for j in range(numberOfCom) ]
 MinFriendsIn = 0.4
 MaxFriendsIn = 0.7
 MinFriendsOut = 0.1
 MaxFriendsOut = 0.3
-threshold = 0.6
+threshold = 0
+typeOfVotes=["A","B","C"]
 winnerStart=''
 winnerFinal=''
-xLengthGraph= 5  # between 2 - 6 types of votes ! dont change !
-Xlegend = "Number of Votes"
+xLengthGraph=10
+Xlegend = "Threshold"
 TotalIter = [0 for a3 in range (xLengthGraph)]
 changeVar = [0 for a4 in range (xLengthGraph)]
-numberOfCom = 0
-WinnerGraph = [ "" for j in range(xLengthGraph)]
-Opinions2 = {}
-change = True
-numberOfCom = 10
-MinPeople = 4
-MaxPeople = 5
+WinnerGraph = [ 0 for i in range(xLengthGraph) ]
+winnerVotes= [0 for i2 in range(len(typeOfVotes))]
+Opinions2={}
 
-sizes = [0 for i in range(numberOfCom)]
-probs = [[0 for i in range(numberOfCom)] for j in range(numberOfCom)]
-probMatrix(numberOfCom, probs, MinFriendsIn, MaxFriendsIn)
-for i in range(numberOfCom):
-    sizes[i] = random.randint(MinPeople, MaxPeople)
-print("size of Coum :")
-print(sizes)
-print("probs :")
-print(np.matrix(probs))
-
-BlockGraph = nx.stochastic_block_model(sizes, probs, seed=364)
-edges = nx.edges(BlockGraph)
-friends = [set() for j in range(len(BlockGraph))]
-
-###############################################################################
-
-'''checking on different size of type Of Votes'''
-for a1 in range (xLengthGraph):
-    change = True
-    numOfIteration=0
-    # change votes length
-
-    typeOfVotes = numberOfVotes(a1+2)
-    print("The Round : ", a1+1, "the", Xlegend, " is ", len(typeOfVotes))
-    winnerVotes = [0 for i2 in range(len(typeOfVotes))]
-    changeVar[a1] = len(typeOfVotes)
-
-    Opinions = creatOpinions(BlockGraph, typeOfVotes, Opinions2, winnerVotes)
-    votes = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
-    votes2 = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
-    winnerStart = getWinner(winnerVotes, typeOfVotes)
-    prints(typeOfVotes, winnerVotes, winnerStart, Opinions2, edges)
+for seedi in range(10):
+    seede= 363+ seedi*140
+    random.seed(seede)
+    print( "######################### next seed , the seed is ", seede , "#################")
     Clean(winnerVotes)
 
-    for x in range(len(BlockGraph)):
-        Countvotes(typeOfVotes, winnerVotes, Opinions2, x)
+    createCom(numberOfCom,MinPeople, MaxPeople)
+    probMatrix(numberOfCom,probs,MinFriendsIn,MaxFriendsIn)
+
+    print("size of Coum :")
+    print(sizes)
+
+    #print("probs :")
+    #print(np.matrix(probs))
+
+    BlockGraph = nx.stochastic_block_model(sizes, probs, seed=seede)
+    edges = nx.edges(BlockGraph)
+    friends= [set() for j in range(len(BlockGraph) )]
+
+    Opinions=creatOpinions(BlockGraph, typeOfVotes, Opinions2, winnerVotes)
+    winnerStart = getWinner (winnerVotes,typeOfVotes)
+    votes = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
+    votes2 = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
+
+    prints (typeOfVotes, winnerVotes, winnerStart, Opinions2, edges)
 
     setFridends(edges, friends)
 
-    print("friends :")
-    print(friends)
+    #print("friends :")
+    #print(friends)
 
-    while(change):
-        friends2 = runInit(friends, votes, votes2)
-        #winnerStart = getWinner(winnerVotes, typeOfVotes)
-        numOfIteration = numOfIteration + 1
-        print( "numOfIteration is : " , numOfIteration)
-        change = False
+    for a1 in range (xLengthGraph):
+        threshold = 0.35 + a1/20
+        changeVar[a1] = threshold
+        Opinions = copy.deepcopy(Opinions2)
+        print()
+        print("The Round : " , a1+1 , "the" , Xlegend , "is : " , threshold )
+        change = True
+        numOfIteration=0
+        while(change):
+            if(max_of_iter==numOfIteration):
+                print("break while")
+                break
+            friends2 = runInit(friends, votes, votes2)
+            numOfIteration = numOfIteration + 1
+            print("numOfIteration is : ", numOfIteration)
+            change = False
 
-        for f in range(len(friends)):
-            counter=0
-            counter = countMyOpinion(typeOfVotes, Opinions, votes, counter, f)
-            counter = countFriendsOpinion(typeOfVotes, Opinions, votes, counter, f)
-            change = percentOfVotes(counter, typeOfVotes, votes2, threshold, Opinions, f, change)
+            for f in range(len(friends)):
+                counter=0
+                counter = countMyOpinion(typeOfVotes, Opinions, votes, counter, f)
+                counter = countFriendsOpinion(typeOfVotes, Opinions, votes, counter, f)
+                change = percentOfVotes(counter, typeOfVotes, votes2, threshold, Opinions, f, change)
 
-    print(np.matrix(Opinions))
-    Clean(winnerVotes)
-    for x1 in range(len(BlockGraph)):
-        Countvotes(typeOfVotes, winnerVotes, Opinions, x1)
+        Clean(winnerVotes)
+        for x1 in range(len(BlockGraph)):
+            Countvotes(typeOfVotes, winnerVotes, Opinions, x1)
 
-    print("final votes")
-    print(np.matrix(typeOfVotes))
-    print(np.matrix(winnerVotes))
-    WinnerGraph[a1] = getWinner(winnerVotes,typeOfVotes)
-    print("Final winner is:",WinnerGraph[a1])
+        WinnerGraph[a1] = getWinner(winnerVotes, typeOfVotes)
 
-    '''print(np.matrix(votes))
+        print("final votes")
+        print(np.matrix(typeOfVotes))
+        print(np.matrix(winnerVotes))
+        print("Final Winner is:",WinnerGraph[a1])
+
+        TotalIter[a1] = numOfIteration
+
     print()
-    print(np.matrix(votes2))
-    print("num of iteration")
-    print(numOfIteration)'''
-    TotalIter[a1] = numOfIteration
-
-    '''plt.show(nx.draw(BlockGraph , pos = nx.spring_layout(BlockGraph)))'''
-    print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Next Round XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print()
-
-print()
-print("End  : ")
-print(np.matrix(changeVar))
-print(np.matrix(TotalIter))
-
-# Threshold plot
-Ylabel= "Number of Iterations"
-CreatePlotGraph (changeVar, TotalIter , "Number of Communities" , Ylabel)
-
-# Winner Plot
-print(np.matrix(WinnerGraph))
-Ylabel= " Final Winner is :"
-CreatescatterGraph (changeVar, WinnerGraph , Xlegend , Ylabel)
-
-
+    print("End  : ")
+    print(np.matrix(changeVar))
+    print(np.matrix(TotalIter))
+'''
+    # מדד אחוז שינוי מנצח (מספר הפעמים שמשתנה המנצח)
+    Ylabel = " Final Winner is :"
+    CreatescatterGraph (changeVar, WinnerGraph , Xlegend , Ylabel)'''
