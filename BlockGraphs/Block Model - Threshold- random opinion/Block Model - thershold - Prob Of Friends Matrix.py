846f9880e4ced0e1b06e93e7e8d2829181882776
Or azar <orazar1805@gmail.com>
2020-12-30 18:32:35 +0200

    update

--- BlockGraphs/Block Model - Threshold- random opinion/Block Model - thershold - Prob Of Friends Matrix.py
+++ BlockGraphs/Block Model - Threshold- random opinion/Block Model - thershold - Prob Of Friends Matrix.py
@@ -45,9 +45,10 @@ def probMatrix(numberOfCom, probs, MinFriendsIn, MaxFriendsIn):
 def creatOpinions(BlockGraph, typeOfVotes, Opinions2, winnerVotes):
     for x in range(len(BlockGraph)):
         Opinions2[x] = random.choice(typeOfVotes)
-        Countvotes(typeOfVotes, winnerVotes, Opinions2, x)
+        Countvotes(typeOfVotes, winnerVotes, Opinions2,x)
     Opinions = copy.deepcopy(Opinions2)
     return Opinions
+
 
 
 def createCom(numberOfCom, MinPeople, MaxPeople):
@@ -160,14 +161,39 @@ def getValue(a):
         x = b
         a.remove(b)
         return x
+def calculate_winner_to_graph(start_winner_graph, end_winner_graph, winner_per_graph ,num_vote ,seedi , a1):
+    winnerEnd = getWinner(end_winner_graph, typeOfVotes)
+    precente = 0
+    if (winnerEnd == "A"):
+        precente=end_winner_graph[0]-start_winner_graph[0]
+    if (winnerEnd == "B"):
+        precente = end_winner_graph[1] - start_winner_graph[1]
+    if (winnerEnd == "C"):
+        precente = end_winner_graph[2] - start_winner_graph[2]
+    if (winnerEnd == "D"):
+        precente = end_winner_graph[2] - start_winner_graph[2]
+    if (winnerEnd == "E"):
+        precente = end_winner_graph[2] - start_winner_graph[2]
+    if (winnerEnd == "F"):
+        precente = end_winner_graph[2] - start_winner_graph[2]
+    precente = abs(precente/num_vote)
+    #print(precente)
+    winner_per_graph[seedi][a1] = precente
 
-max_of_iter= 10
-numberOfCom = 20
-MinPeople = 45
-MaxPeople = 55
+def calculate_winner_change_to_graph(start_change_winner_graph, end_winner_graph, winner_change_graph ,seedi , a1):
+    winnerEnd = getWinner(end_winner_graph, typeOfVotes)
+    if (winnerEnd == start_change_winner_graph):
+        winner_change_graph[seedi][a1] = 0
+    else:
+        winner_change_graph[seedi][a1] = 1
+
+number_of_seeds = 50
+max_of_iter= 15
+numberOfCom = 5
+MinPeople = 20
+MaxPeople = 25
 sizes = [ 0 for i in range(numberOfCom) ]
 probs = [ [ 0 for i in range(numberOfCom) ] for j in range(numberOfCom) ]
-
 threshold = 0.5
 typeOfVotes=["A","B","C"]
 winnerStart=''
@@ -180,7 +206,18 @@ WinnerGraph = [ 0 for i in range(xLengthGraph) ]
 winnerVotes= [0 for i2 in range(len(typeOfVotes))]
 Opinions2={}
 
-for seedi in range(10):
+GarphIter = [[0 for j in range(xLengthGraph)] for i in range(number_of_seeds)]
+GarphIter_avg = [0 for j in range(xLengthGraph)]
+
+start_winner_graph = [0 for j in range(len(typeOfVotes))]
+winner_per_graph = [[0 for j in range(xLengthGraph)] for i in range(number_of_seeds)]
+winner_per_graph_avg = [0 for j in range(xLengthGraph)]
+
+start_change_winner_graph = "T"
+winner_change_graph = [[0 for j in range(xLengthGraph)] for i in range(number_of_seeds)]
+winner_change_graph_avg = [0 for j in range(xLengthGraph)]
+
+for seedi in range(number_of_seeds):
     seede= 363+ seedi*140
     random.seed(seede)
     print( "######################### next seed , the seed is ", seede , "#################")
@@ -202,13 +239,15 @@ for seedi in range(10):
         MaxFriendsIn = 0.4 + a1 / 20
         MinFriendsOut = 0.01 + a1 / 50
         MaxFriendsOut = 0.1 + a1 / 50
+        changeVar[a1] = MinFriendsIn
         probMatrix(numberOfCom, probs, MinFriendsIn, MaxFriendsIn)
         BlockGraph = nx.stochastic_block_model(sizes, probs, seed=seede)
         edges = nx.edges(BlockGraph)
         friends = [set() for j in range(len(BlockGraph))]
         Opinions = creatOpinions(BlockGraph, typeOfVotes, Opinions2, winnerVotes)
         winnerStart = getWinner(winnerVotes, typeOfVotes)
-
+        start_change_winner_graph = copy.deepcopy(winnerStart)
+        start_winner_graph = copy.deepcopy(winnerVotes)
         votes = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
         votes2 = [[0 for i in range(len(typeOfVotes))] for j in range(len(friends))]
 
@@ -253,8 +292,69 @@ for seedi in range(10):
         print("Final Winner is:",WinnerGraph[a1])
 
         TotalIter[a1] = numOfIteration
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Next Round XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        print()
+        calculate_winner_to_graph(start_winner_graph, winnerVotes, winner_per_graph, len(BlockGraph), seedi, a1)
+        calculate_winner_change_to_graph(start_change_winner_graph, winnerVotes, winner_change_graph, seedi, a1)
 
+    for i in range(len(TotalIter)):
+        GarphIter[seedi][i] = TotalIter[i]
     print()
     print("End  : ")
     print(np.matrix(changeVar))
     print(np.matrix(TotalIter))
+
+# Number of Iterations VS number of com plot
+print("Number of Iterations VS number of com plot")
+print(np.matrix(GarphIter))
+for i in range (number_of_seeds):
+    for j in range(len(TotalIter)):
+        GarphIter_avg[j] = GarphIter_avg[j] + GarphIter[i][j]
+
+print(np.matrix(GarphIter_avg))
+
+for i in range(len(TotalIter)):
+    GarphIter_avg[i] = round(GarphIter_avg[i]/number_of_seeds,3)
+
+print(np.matrix(GarphIter_avg))
+
+Ylabel= "Number of Iterations"
+CreatePlotGraph (changeVar, GarphIter_avg , "Number of Communities" , Ylabel)
+
+
+# Winner Present Votes VS number of com plot
+print("Winner Present Votes VS number of com plot")
+print(np.matrix(winner_per_graph))
+
+for i in range (number_of_seeds):
+    for j in range(len(TotalIter)):
+        winner_per_graph_avg[j] = winner_per_graph_avg[j] + winner_per_graph[i][j]
+print()
+print(np.matrix(winner_per_graph_avg))
+
+for i in range(len(TotalIter)):
+    winner_per_graph_avg[i] = round(winner_per_graph_avg[i]/number_of_seeds,3)
+print()
+print(np.matrix(winner_per_graph_avg))
+
+
+Ylabel= "Diff Between Start to End Present Winner Votes"
+CreatePlotGraph (changeVar, winner_per_graph_avg , "Number of Communities" , Ylabel)
+
+# Winner change VS number of com plot
+print("Winner change VS number of com plot")
+print(np.matrix(winner_change_graph))
+
+for i in range (number_of_seeds):
+    for j in range(len(TotalIter)):
+        winner_change_graph_avg[j] = winner_change_graph_avg[j] + winner_change_graph[i][j]
+
+print(np.matrix(GarphIter_avg))
+
+for i in range(len(TotalIter)):
+    winner_change_graph_avg[i] = round(winner_change_graph_avg[i]/number_of_seeds,3)
+
+print(np.matrix(winner_change_graph_avg))
+
+Ylabel= "Winner change in %"
+CreatePlotGraph (changeVar, winner_change_graph_avg , "Number of Communities" , Ylabel)
